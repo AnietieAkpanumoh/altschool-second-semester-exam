6baff54eb5b29ef8e0f624e557e2f7df3a306698
Anietie Akpanumoh <anietieakpanumoh1@gmail.com>
2022-11-04 03:12:02 +0100

    ErrorBoundary

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
+import "./App.css";
+import Home from "./pages/Home";
+import ApiFetch from "./components/ApiFetch";
+import Nested from "./components/Nested";
+import Navbar from "./components/Navbar";
+import ErrorPage from "./pages/ErrorPage";
 
 function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="repositories" element={<ApiFetch />} />
+                <Route path="repositories/:reponame" element={<Nested />} />
+                <Route path="*" element={<ErrorPage/>} /> 
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
